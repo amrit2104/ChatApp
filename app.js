f99479ff15817148c8d8c2e36cfca1a91aff8ef2
Amrit Gaurav Rath <amrit2104@gmail.com>
2024-07-13 20:43:11 +0530

    project upload part-3: Client update

--- app.js
+++ app.js
@@ -4,4 +4,26 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`chat server on port ${PORT}`))
 
+// initiating web server with socket.io
+const io = require('socket.io')(server)
+
 app.use(express.static(path.join(__dirname,'public')))
+
+let socketsConnected = new Set()
+
+// listening to an event on the web socket server that is connection event
+io.on('connection', onConnected)
+
+// Let's increase the number of clients now
+function onConnected(socket) {
+    console.log(socket.id)
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total', socketsConnected.size)
+// removing a socket if a client closes connection.
+    socket.on('disconnect', () => {
+        console.log('Socket disconnected', socket.id)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    })
+}
