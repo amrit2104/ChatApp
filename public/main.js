9a6910399a3691d7f912ba470116b551ae941773
Amrit Gaurav Rath <amrit2104@gmail.com>
2024-07-14 17:28:40 +0530

    corrected main.js

--- public/main.js
+++ public/main.js
@@ -41,7 +41,7 @@ function addMessageToUi(isOwnMessage, data) {
             <li class="${isOwnMessage ? "message-right" : "message-left"}">
                 <p class="message">
                     ${data.message}!
-                    <span>{data.name} ● {moment(data.dateTime).fromNow()}</span>
+                    <span>${data.name} ● {moment(data.dateTime).fromNow()}</span>
                 </p>
             </li>
             `
