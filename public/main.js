af9542504afbb28172b58f952b9c1788b991d03e
Amrit Gaurav Rath <amrit2104@gmail.com>
2024-07-14 17:19:50 +0530

    corrected main.js

--- public/main.js
+++ public/main.js
@@ -1,90 +1,49 @@
+// Lets try to make a connection to web socket server.
+
 const socket = io()
+const clientsTotal = document.getElementById('clients-total')
 
-const clientsTotal = document.getElementById('client-total')
-
+//we will enable message sending
 const messageContainer = document.getElementById('message-container')
 const nameInput = document.getElementById('name-input')
 const messageForm = document.getElementById('message-form')
 const messageInput = document.getElementById('message-input')
 
-const messageTone = new Audio('/message-tone.mp3')
-
+//adding an event
 messageForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  sendMessage()
+    e.preventDefault() //prevents reloading of the page.
+    sendMessage()
 })
 
-socket.on('clients-total', (data) => {
-  clientsTotal.innerText = `Total Clients: ${data}`
+socket.on(`clients-total`, (data) => {
+    clientsTotal.innerText = `Total Clients: ${data}`
 })
 
 function sendMessage() {
-  if (messageInput.value === '') return
-  // console.log(messageInput.value)
-  const data = {
-    name: nameInput.value,
-    message: messageInput.value,
-    dateTime: new Date(),
-  }
-  socket.emit('message', data)
-  addMessageToUI(true, data)
-  messageInput.value = ''
+    console.log(messageInput.value)
+    const data = {
+        name: nameInput.value,
+        message: messageInput.value,
+        dateTime: new Date()
+    }
+    socket.emit('message', data) //for sending the message using socket
+    addMessageToUi(true,data)
+    messageInput.value = ''
 }
 
-socket.on('chat-message', (data) => {
-  // console.log(data)
-  messageTone.play()
-  addMessageToUI(false, data)
+socket.on('chat-message', (date) => {
+    // console.log(data)
+    addMessageToUi(false,data)
 })
 
-function addMessageToUI(isOwnMessage, data) {
-  clearFeedback()
-  const element = `
-      <li class="${isOwnMessage ? 'message-right' : 'message-left'}">
-          <p class="message">
-            ${data.message}
-            <span>${data.name} ● ${moment(data.dateTime).fromNow()}</span>
-          </p>
-        </li>
-        `
-
-  messageContainer.innerHTML += element
-  scrollToBottom()
-}
-
-function scrollToBottom() {
-  messageContainer.scrollTo(0, messageContainer.scrollHeight)
-}
-
-messageInput.addEventListener('focus', (e) => {
-  socket.emit('feedback', {
-    feedback: `✍️ ${nameInput.value} is typing a message`,
-  })
-})
-
-messageInput.addEventListener('keypress', (e) => {
-  socket.emit('feedback', {
-    feedback: `✍️ ${nameInput.value} is typing a message`,
-  })
-})
-messageInput.addEventListener('blur', (e) => {
-  socket.emit('feedback', {
-    feedback: '',
-  })
-})
-
-socket.on('feedback', (data) => {
-  clearFeedback()
-  const element = `
-        <li class="message-feedback">
-          <p class="feedback" id="feedback">${data.feedback}</p>
-        </li>
-  `
-  messageContainer.innerHTML += element
-})
-
-function clearFeedback() {
-  document.querySelectorAll('li.message-feedback').forEach((element) => {
-    element.parentNode.removeChild(element)
-  })
+function addMessageToUi(isOwnMessage, data) {
+    const element = `
+            <li class="${isOwnMessage ? "message-right" : "message-left"}">
+                <p class="message">
+                    ${data.message}!
+                    <span>{data.name} ● {moment(data.dateTime).fromNow()}</span>
+                </p>
+            </li>
+            `
+    messageContainer
 }
